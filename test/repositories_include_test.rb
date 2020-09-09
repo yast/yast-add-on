605a1cebe9f388dbb1cea1535de6c14aefd0cae0
Ladislav Slezák <lslezak@suse.cz>
2020-09-09 16:59:56 +0200

    Display the add-on selection dialog even when a DUD add-on is present (bsc#1174562)

--- test/repositories_include_test.rb
+++ test/repositories_include_test.rb
@@ -130,4 +130,46 @@ describe "Yast::AddOnWorkflowInclude" do
       AddonIncludeTester.RunAddProductWorkflow
     end
   end
+
+  describe "#RunAddOnMainDialog" do
+    before do
+      allow(Yast::Pkg).to receive(:SourceReleaseAll)
+      allow(Yast::Stage).to receive(:initial).and_return(true)
+      allow(AddonIncludeTester).to receive(:HasInsufficientMemory).and_return(false)
+      allow(Yast::WorkflowManager).to receive(:SetBaseWorkflow)
+      allow(Yast::Wizard).to receive(:SetTitleIcon)
+      allow(AddonIncludeTester).to receive(:Redraw)
+      allow(Yast::AddOnProduct).to receive(:PrepareForRegistration)
+      allow(Yast::AddOnProduct).to receive(:Integrate)
+      allow(Yast::AddOnProduct).to receive(:ProcessRegistration)
+      allow(Yast::AddOnProduct).to receive(:ReIntegrateFromScratch)
+      allow(Yast::Wizard).to receive(:RestoreBackButton)
+      allow(Yast::Wizard).to receive(:RestoreAbortButton)
+      allow(Yast::Wizard).to receive(:RestoreNextButton)
+      allow(Yast::UI).to receive(:UserInput).and_return(:next)
+    end
+
+    context "a DUD add-on present, using Full medium" do
+      before do
+        allow(Yast::AddOnProduct).to receive(:add_on_products).and_return([
+          {
+            "media" => 4,
+            "product" => "Driver Update 0",
+            "autoyast_product" => "Driver Update 0",
+            "media_url" => "dir:///update/000/repo?alias=DriverUpdate0",
+            "product_dir" => "",
+            "priority" => 50
+          }
+        ])
+        allow(Y2Packager::MediumType).to receive(:offline?).and_return(true)
+      end
+
+      it "asks for the media addons and stores the state" do
+        expect(AddonIncludeTester).to receive(:RunWizard).and_return(:next)
+        expect { AddonIncludeTester.RunAddOnMainDialog(true, true, true, "", "", "", true) }.to \
+          change { Yast::AddOnAddOnWorkflowInclude.class_variable_get(:@@media_addons_selected) } \
+          .from(false).to(true)
+      end
+    end
+  end
 end
